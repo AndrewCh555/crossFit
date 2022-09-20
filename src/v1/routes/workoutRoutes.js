cc08adaf5fd90c32d57ba88eff76d2a062334095
Andrew Chemes <andrey.chemes@gmail.com>
2022-09-20 12:02:33 +0300

    first commit

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+// *** ADD ***
+const apicache = require("apicache");
+const workoutController = require("../../controllers/workoutController");
+// const recordController = require("../../controllers/recordController");
+
+const router = express.Router();
+
+const cache = apicache.middleware;
+const authenticate = require("../../middlewares/authenticate");
+const authorize = require("../../middlewares/authorize");
+
+router.post("/", authenticate, authorize, workoutController.createNewWorkout);
+
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
+
+router.get("/:workoutId", workoutController.getOneWorkout);
+
+// router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
+router.post("/", workoutController.createNewWorkout);
+
+router.patch("/:workoutId", workoutController.updateOneWorkout);
+
+router.delete("/:workoutId", workoutController.deleteOneWorkout);
+
+module.exports = router;
